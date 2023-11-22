12d72b36974dabfbd16e4aec1d4f57d290165d21
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-11-22 16:19:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ for (let i = 0; i < priceArray.length; i++) {
 	ans+=parseInt(priceArray[i].innerText);
 }
 let total=document.createElement("tr");
-let td=document.createElement("td");
-total.setAttribute("data-ns-test", "prices");
-// td.setAttribute("data-ns-test", "prices");
-td.innerText=ans;
-total.appendChild(td);
+
+total.setAttribute("data-ns-test", "grandTotal");
+
+total.innerText=ans;
+
 table.appendChild(total);
